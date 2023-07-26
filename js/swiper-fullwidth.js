f4c4c914363ea9734a9e3303ccf02be0672984ac
ASTIK <astik.ytt@gmail.com>
2023-07-26 19:19:23 +0200

    Add .swiper-fullwidth

---
+++ js/swiper-fullwidth.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-fullwidth__nav_next',
+    prevEl: '.swiper-fullwidth__nav_prev',
+  },
+
+  resistanceRatio: 0,
+});
