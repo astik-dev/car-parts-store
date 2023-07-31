2dcb706649ea6c667c94de99fe83179e1a3ae3ef
ASTIK <astik.ytt@gmail.com>
2023-07-31 14:24:09 +0200

    Update swiper nav buttons

--- js/swiper-fullwidth.js
+++ js/swiper-fullwidth.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         items: 1,
         dots: false,
         pullDrag: false,
-        navText: [`<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M11.0001 6L7.00012 10L11.0001 14" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>`, `<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M8.99988 6L12.9999 10L8.99988 14" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/> </svg>`],
+        navText: [`<img src="img/swiper_prev-btn.svg" alt="Swiper button">`, `<img src="img/swiper_prev-btn.svg" alt="Swiper button">`],
         dragEndSpeed: 500,
         navSpeed: 500,
     });
